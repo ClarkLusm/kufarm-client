ebfa04182841bd79b28c516f55869eb73f6dbdf9
Thanh Phan <phanthanh.humg@gmail.com>
2024-09-20 08:24:35 +0700

    Update default wallet show on walletconnect

--- contexts/web3.context.tsx
+++ contexts/web3.context.tsx
@@ -1,21 +1,21 @@
-'use client'
+"use client";
 
-import { NETWORKS } from '@/libs/constants'
-import { createWeb3Modal, defaultConfig } from '@web3modal/ethers/react'
+import { NETWORKS } from "@/libs/constants";
+import { createWeb3Modal, defaultConfig } from "@web3modal/ethers/react";
 
 // 1. Get projectId from https://cloud.walletconnect.com
-const projectId = process.env.WALLET_CONNECT_PROJECT_ID || ''
+const projectId = process.env.WALLET_CONNECT_PROJECT_ID || "";
 
 // 2. Set chains
-const chains = Object.values(NETWORKS)
+const chains = Object.values(NETWORKS);
 
 // 3. Create a metadata object
 const metadata = {
-  name: 'Bitcoino2fi',
-  description: 'Bitcoino2fi description',
-  url: process.env.NEXTAUTH_URL ?? '', // origin must match your domain & subdomain
-  icons: ['http://bitcoino2fi.com/']
-}
+  name: "Bitcoino2fi",
+  description: "Bitcoino2fi description",
+  url: process.env.NEXTAUTH_URL ?? "", // origin must match your domain & subdomain
+  icons: ["http://bitcoino2fi.com/"],
+};
 // 4. Create Ethers config
 const ethersConfig = defaultConfig({
   /*Required*/
@@ -27,7 +27,7 @@ const ethersConfig = defaultConfig({
   enableCoinbase: false, // true by default
   // rpcUrl: '...', // used for the Coinbase SDK
   // defaultChainId: 1 // used for the Coinbase SDK
-})
+});
 
 // 5. Create a AppKit instance
 createWeb3Modal({
@@ -36,8 +36,12 @@ createWeb3Modal({
   projectId,
   // enableAnalytics: true // Optional - defaults to your Cloud configuration
   allowUnsupportedChain: false,
-})
+  includeWalletIds: [
+    "0b415a746fb9ee99cce155c2ceca0c6f6061b1dbca2d722b3ba16381d0562150", // Safepal
+    "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96", // Metamask
+  ],
+});
 
 export function AppKit({ children }: any) {
-  return children
+  return children;
 }
